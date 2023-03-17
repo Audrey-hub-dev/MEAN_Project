f716859ef637c9027965b1f379287ab8178fc2ec
Audrey <audrey@MacBook-Pro-de-Audrey.local>
2023-03-17 19:18:45 +0100

    Chapitre 22: Using the Angular HTTP Client

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Post } from './post.model';
+import { HttpClient } from '@angular/common/http';
 
 
 @Injectable({providedIn: 'root'})
@@ -8,8 +9,15 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
+  constructor(private http:HttpClient) {}
+
   getPosts() {
-    return [...this.posts];
+    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        this.postsUpdated.next([...this.posts]);
+
+      });
   }
 
   getPostUpdateListener() {
@@ -17,12 +25,7 @@ export class PostsService {
   }
 
   addPost(title: string, content: string) {
-    //le premier post est le nom de l'objet que l'on donne
-    //le deuxième Post après les deux points et avec une majuscule
-    //est le type que l'on donne
-    const post: Post = {title: title, content: content};
-    //avec le this on va atteindre notre tableau de posts et y pousser le nouveau
-    //post dans ce tableau
+    const post: Post = { id: null, title: title, content: content};
     this.posts.push(post);
     this.postsUpdated.next([...this.posts]);
 
