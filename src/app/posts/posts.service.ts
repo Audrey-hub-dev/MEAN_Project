02ba84e034ee5baddb8412844043ccca1a87b6bf
Audrey <audrey@MacBook-Pro-de-Audrey.local>
2023-03-21 17:35:03 +0100

    Chapitre 33 : Deleting documents

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -44,6 +44,14 @@ export class PostsService {
         this.postsUpdated.next([...this.posts]);
       });
   }
+
+  deletePost(postId: string) {
+    this.http.delete("http://localhost:3000/api/posts/" + postId)
+      .subscribe(() => {
+        console.log('Deleted!');
+      });
+  }
+
 }
 
 
