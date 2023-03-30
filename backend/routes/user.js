3f5e31edca0792d14ef0db1794b9e3f8738ef6ed
Audrey <audrey@MacBook-Pro-de-Audrey.local>
2023-03-30 09:18:20 +0200

    Chapitre 66: Sending the token to the front-end

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -32,6 +32,7 @@ router.post('/signup', (req, res, next) => {
   });
 
 router.post('/login', (req, res, next) => {
+  let fetchedUser;
   User.findOne({ email: req.body.email })
     .then(user => {
       if (!user) {
@@ -39,6 +40,7 @@ router.post('/login', (req, res, next) => {
           message: 'Auth failed'
         });
       }
+      fetchedUser = user;
       return bcrypt.compare(req.body.password, user.password);
     })
     .then(result => {
@@ -48,10 +50,13 @@ router.post('/login', (req, res, next) => {
         });
       }
       const token = jwt.sign(
-        {email: user.email, userId: user._id},
+        {email: fetchedUser.email, userId: fetchedUser._id},
         'secret_this_should_be_longer',
         { expiresIn: "1h" }
       );
+      res.status(200).json({
+        token: token
+      });
     })
     .catch(err => {
       return res.status(401).json({
