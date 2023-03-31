05494cff00d101385d484a941d6dd0d5ee4583a3
Audrey <audrey@MacBook-Pro-de-Audrey.local>
2023-03-31 17:02:39 +0200

    Chapitre 73 : Reflecting the token expiration in the UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,19 +6,21 @@ import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { PostListComponent } from "./posts/post-list/post-list.component";
 import { LoginCommponent } from "./auth/login/login.component";
 import { SignupCommponent } from "./auth/signup/signup.component";
+import { AuthGuard } from "./auth/auth.guard";
 
 
 const routes: Routes = [
   { path: '', component: PostListComponent },
-  { path: 'create', component: PostCreateComponent },
-  { path: 'edit/:postId', component: PostCreateComponent },
+  { path: 'create', component: PostCreateComponent, canActivate:[AuthGuard] },
+  { path: 'edit/:postId', component: PostCreateComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginCommponent },
   { path: 'signup', component: SignupCommponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 
 })
 
