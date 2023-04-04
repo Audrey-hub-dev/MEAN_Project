969250615272af9cb20e6f4acd501fa3be2f4f65
Audrey <audrey@MacBook-Pro-de-Audrey.local>
2023-04-04 10:44:45 +0200

    Chapitre 79 : Using the User ID on the Frontend

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -37,6 +37,7 @@ export class PostListComponent implements OnInit, OnDestroy {
  ngOnInit() {
   this.isLoading = true
   this.postsService.getPosts(this.postsPerPage, this.currentPage);
+  this.userId = this.authService.getUserId();
   this.postsSub = this.postsService.getPostUpdateListener()
     .subscribe((postData: { posts: Post[], postCount: number}) => {
       this.isLoading = false;
@@ -48,6 +49,7 @@ export class PostListComponent implements OnInit, OnDestroy {
       .getAuthStatusListener()
       .subscribe(isAuthenticated => {
         this.userIsAuthenticated = isAuthenticated;
+        this.userId = this.authService.getUserId();
 
     } );
  }
